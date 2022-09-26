070df9e0e5f5e28fa6c872d5915244dfd5fc1820
calicomills <jishnuck@gmail.com>
2022-09-26 15:25:14 +0530

    commit-all

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Agent, Lead
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Agent)
+admin.site.register(Lead)
